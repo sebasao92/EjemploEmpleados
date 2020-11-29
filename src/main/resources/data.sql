9e5c12828e484e4459d0be0273c7d8c4f53479dc
Sebastián <sebastian.alarcon@nordstrom.com>
2020-11-28 20:08:36 -0500

    mejores en el properties y en el data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO empleado (cedula, nombre, salario)
-VALUES ('1234', 'Sebas', 2345.0),
-       ('1235', 'Yusep', 56700004.0),
-       ('1236', 'Luis', 353245345.0),
-       ('1237', 'Jairo', 543535543.0);
+VALUES ('1', 'Sebas', 2345.0),
+       ('2', 'Yusep', 56700004.0),
+       ('3', 'Luis', 353245345.0),
+       ('4', 'Jairo', 543535543.0);
